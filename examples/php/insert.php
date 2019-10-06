2a6a54c4b3bf8df0295931fbd3b334c42bc84368
maochichi <stefanmao1112@gmail.com>
2019-10-06 22:34:46 +0800

    1.完成動態新增 基本功能. (MySQL 也會同步更新)

--- examples/php/insert.php
+++ examples/php/insert.php
@@ -1,27 +1,16 @@
 <?php
 //insert.php
+//header("Content-Type:text/html;charset=BIG5");
 require_once("DB_config.php");
 
-  try
-{
     //注意，使用PDO方式連結，需要指定一個資料庫，否則將拋出異常
-    $conn = new PDO('mysql:host=localhost;dbname=fullcalendar','root','5566');
-    $conn->exec("SET CHARACTER SET utf8");
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "insert.php Connected Successfully"."<br />";
-}
-catch(PDOException $e)
-{
-    echo "insert.php Connection failed: ".$e->getMessage()."<br />";
-}
+$connect = new PDO('mysql:host=localhost;dbname=fullcalendar;charset=utf8mb4','root','5566');
+$connect->exec("SET CHARACTER SET utf8");
+$connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
 if(isset($_POST["title"]))
 {
- 	
- print_r($_POST);
- $query = "
- INSERT INTO events 
- (title, start_event, end_event) 
- VALUES (:title, :start_event, :end_event)";
+ $query = "INSERT INTO events (title, start_event, end_event) VALUES (:title, :start_event, :end_event) ";
  $statement = $connect->prepare($query);
  $statement->execute(
   array(
@@ -30,6 +19,9 @@ if(isset($_POST["title"]))
    ':end_event' => $_POST['end']
   )
  );
+
+ print_r($_POST);
 }
+
 
 ?>
